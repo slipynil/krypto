a5648525210b86276499401c678e3cb368f88e6f
Vladimir Aristarkhov <slipynil@gmail.com>
2026-06-28 05:50:26 +0300

    feat: add list.Item implementation to Coin

--- internal/models/coin.go
+++ internal/models/coin.go
@@ -6,3 +6,15 @@ type Coin struct {
 	Name      string            `json:"name"`
 	Platforms map[string]string `json:"platforms"`
 }
+
+func (c Coin) Title() string {
+	return c.Symbol
+}
+
+func (c Coin) Description() string {
+	return c.Name
+}
+
+func (c Coin) FilterValue() string {
+	return c.Name
+}

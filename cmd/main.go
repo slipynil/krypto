80df98c02dd2e34cdb46768a750959c42448a8c0
Vladimir Aristarkhov <slipynil@gmail.com>
2026-06-28 06:10:24 +0300

    feat: add flag curr

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
+	tea "charm.land/bubbletea/v2"
+
 	"github.com/joho/godotenv"
-	"github.com/k0kubun/pp"
 	"github.com/slipynil/krypto/internal/api"
+	"github.com/slipynil/krypto/internal/manager"
 	"github.com/slipynil/krypto/internal/models"
+	"github.com/slipynil/krypto/internal/storage"
+	"github.com/slipynil/krypto/internal/tui"
 )
 
 func main() {
-	apiKey := loadEnv()
-	apiService := api.NewService(apiKey, models.CURRENCY_RUB)
-	result, err := apiService.GetPriceInfo("bitcoin")
-	if err != nil {
-		log.Fatal(err)
+	p := tea.NewProgram(setupTui())
+
+	if _, err := p.Run(); err != nil {
+		panic(err)
 	}
-	pp.Println(result)
+}
+
+func setupTui() tui.Model {
+	apiKey := loadEnv()
+	apiSvc := api.NewService(apiKey)
+	storageSvc, _ := storage.NewService()
+	mgr := manager.New(apiSvc, storageSvc)
+	return *tui.NewModel(mgr, getCurr())
+}
+
+func getCurr() models.Currency {
+	currency := flag.String("curr", "USD", "флаг для валюты, в которой будет все считаться\nexpample: krypto --curr RUB")
+	flag.Parse()
+	return models.Currency(*currency)
 }
 
 func loadEnv() string {

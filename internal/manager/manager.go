92542ec7b4d089a29cf3e6f8be630697e19658b9
Vladimir Aristarkhov <slipynil@gmail.com>
2026-06-28 08:27:21 +0300

    fix(storage): resolve security vulnerability in data handling

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,8 +1,6 @@
 package manager
 
 import (
-	"strings"
-
 	"github.com/slipynil/krypto/internal/api"
 	"github.com/slipynil/krypto/internal/models"
 	"github.com/slipynil/krypto/internal/storage"
@@ -22,32 +20,18 @@ func New(apiSvc *api.ApiService, storageSvc *storage.StorageService) *Manager {
 
 // GetCoins - метод, который скрывает сложность от main.go
 func (m *Manager) GetCoins() ([]models.Coin, error) {
-	coins, err := m.api.GetCryptoIDs()
+	coins, err := m.storage.LoadData()
+
+	if err == nil && len(coins) > 0 {
+		return coins, nil
+	}
+
+	coins, err = m.api.GetCryptoIDs()
 	if err != nil {
 		return nil, err
 	}
-
 	err = m.storage.SaveData(coins)
 	return coins, err
-}
-
-// FindCoins метод ищет подходящюю криптовалюту по запросу
-func (m *Manager) FindCoins(query string) ([]models.Coin, error) {
-	allCoins, err := m.GetCoins() // Берет из кеша или API
-	if err != nil {
-		return nil, err
-	}
-
-	var result []models.Coin
-	for _, coin := range allCoins {
-		if strings.Contains(strings.ToLower(coin.ID), strings.ToLower(query)) {
-			result = append(result, coin)
-		}
-		if len(result) >= 20 { // Ограничиваем вывод 20-ю результатами
-			break
-		}
-	}
-	return result, nil
 }
 
 // выводит информацию о криптовалюте
